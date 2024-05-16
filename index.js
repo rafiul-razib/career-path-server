d6a1fefd258c652a1d5f885c348eb03f8b3eab79
rafiul-razib <rafiul.rzb1@gmail.com>
2024-05-16 21:54:35 +0600

    logout api added

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
+
+const verifyToken = async(req, res, next)=>{
+  const token = req?.cookies?.token;
+  if(!token){
+   return res.status(401).send({message : "Not Authorized"})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({message: "Not Authorized"})
+    }
+    console.log("Value in the token", decoded)
+    req.user = decoded;
+    next()
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fuqgtsz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -71,11 +92,20 @@ async function run() {
       res.send(result)
     })
 
-    app.get("/myJobs", async(req, res)=>{
+    app.get("/myJobs", verifyToken, async(req, res)=>{
+      console.log("query", req.query.email);
+      console.log("token owner", req.user.email)
+      if(req.query.email !== req.user.email){
+        return res.status(403).send({message: "Forbidden Access!!"})
+      }
       let query = {};
       if(req.query?.email){
         query = {user: req.query.email}
       }
+      // console.log(query)
+      // console.log("cook cook",req?.cookies?.token)
+      // console.log("token owner info", req.user)
+     
       const result = await jobCollection.find(query).toArray();
         res.send(result)
     })
@@ -84,6 +114,27 @@ async function run() {
       const job = req.body;
       const result = await jobCollection.insertOne(job);
       res.send(result)
+    })
+
+    app.post("/logout", async(req, res)=>{
+      const user = req.body;
+      console.log("logging out", user);
+      res.clearCookie('token', {maxAge: 0}).send({success:true})
+    })
+
+    app.get("/appliedJob", verifyToken, async(req, res)=>{
+      console.log("query", req.query.email);
+      console.log("token owner", req.user.email);
+      if(req.query.email !== req.user.email){
+        return res.status(403).send({message: "Forbidden Access!!"})
+      }
+      // console.log(req.user.email)
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await appliedJobsCollection.find(query).toArray();
+      res.send(result);
     })
 
     app.post("/appliedJob/:id", async(req, res)=>{
@@ -97,6 +148,18 @@ async function run() {
       const result = await appliedJobsCollection.insertOne(job);
       
       res.send(result)
+    })
+
+    app.post("/jwt", async(req, res)=>{
+      const user = req.body;
+      console.log("user of the token",user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: 3600});
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+      })
+      .send({success : true})
     })
 
     app.delete("/myJobs/:id", async(req, res)=>{
